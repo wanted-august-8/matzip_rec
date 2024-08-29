52934746acd929306e5be3d2663bf0d41697e1e8
hongggs <hongseoda@gmail.com>
2024-08-29 21:20:40 +0900

    Feat: #7 Add 유효성 검사 예외 처리 로직 GlobalExceptionHandler에 추가
    - MethodArgumentNotValidException 처리를 위한 handleValidationExceptions 메서드 추가
    - 유효성 검사 실패 시 INVALID_INPUT_VALUE ErrorCode와 결합된 오류 메시지 반환

--- src/main/java/com/matzip/api/matzip_api/global/error/ErrorCode.java
+++ src/main/java/com/matzip/api/matzip_api/global/error/ErrorCode.java
@@ -1,10 +1,13 @@
 package com.matzip.api.matzip_api.global.error;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 @Getter
 public enum ErrorCode {
+    INVALID_INPUT_VALUE(BAD_REQUEST, "유효하지 않은 입력 값입니다."),
     USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "사용자를 찾을 수 없습니다.");
 
     private final HttpStatus httpStatus;
