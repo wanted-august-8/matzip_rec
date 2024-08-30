0a79979539bc8d154afba038339ba8127bfa95e3
eter2 <eter2hy@gmail.com>
2024-08-30 22:23:34 +0900

    Feat: 중복 데이터 검사 추가

--- src/main/java/com/matzip/api/matzip_api/domain/restrt/repository/RestrtRepository.java
+++ src/main/java/com/matzip/api/matzip_api/domain/restrt/repository/RestrtRepository.java
@@ -1,8 +1,9 @@
 package com.matzip.api.matzip_api.domain.restrt.repository;
 
 import com.matzip.api.matzip_api.domain.restrt.entity.Restrt;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RestrtRepository extends JpaRepository<Restrt, Long> {
-
+    Optional<Restrt> findByRestrtNm(String restrtNm);
 }
