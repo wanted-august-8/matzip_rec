fc4e85937242981847d898db33855fed96417b99
eter2 <eter2hy@gmail.com>
2024-08-30 22:12:11 +0900

    Refactor: 코드 수정

--- src/main/java/com/matzip/api/matzip_api/domain/restrt/controller/RestrtController.java
+++ src/main/java/com/matzip/api/matzip_api/domain/restrt/controller/RestrtController.java
@@ -1,11 +1,6 @@
 package com.matzip.api.matzip_api.domain.restrt.controller;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.matzip.api.matzip_api.domain.restrt.entity.Restrt;
 import com.matzip.api.matzip_api.domain.restrt.service.RestrtService;
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,11 +16,7 @@ public class RestrtController {
 
     @PostMapping("/fetch")
     public ResponseEntity<String> fetchData(@RequestBody String json) throws Exception {
-        try {
-            restrtService.fetchData();
-            return ResponseEntity.ok("fetch data successfully.");
-        } catch (Exception e) {
-            return ResponseEntity.internalServerError().body("Error occurred while fetching and saving restaurants: " + e.getMessage());
-        }
+        restrtService.fetchData();
+        return ResponseEntity.ok("fetch data successfully.");
     }
 }
