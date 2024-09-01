fc2feab3fcb6dd00b0be780318f05048a2c298c8
eter2 <45654545+eter2@users.noreply.github.com>
2024-09-01 19:43:14 +0900

    Revert "Feat: #15 데이터 삽입 자동화"

--- src/main/java/com/matzip/api/matzip_api/domain/restrt/repository/RestrtRepository.java
+++ src/main/java/com/matzip/api/matzip_api/domain/restrt/repository/RestrtRepository.java
@@ -1,19 +1,14 @@
 package com.matzip.api.matzip_api.domain.restrt.repository;
 
 import com.matzip.api.matzip_api.domain.restrt.entity.Restrt;
-import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface RestrtRepository extends JpaRepository<Restrt, Long> {
-    Optional<Restrt> findByRestrtNm(String restrtNm);
-
-    @Query("SELECT r.restrtNm FROM Restrt r")
-    List<String> findAllRestrtNms();
-}
 
 
-
+    // Restrt와 연관된 reviews 및 reviews의 User를 함께 가져오는 메서드
+    @Query("SELECT r FROM Restrt r LEFT JOIN FETCH r.reviews rev LEFT JOIN FETCH rev.user WHERE r.id = :id ORDER BY rev.createdAt DESC")
+    Optional<Restrt> findWithReviewsAndUsersById(Long id);}
