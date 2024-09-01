3cebecede1b4d3226bac144d0770b1c5f500dbea
eter2 <eter2hy@gmail.com>
2024-09-01 22:27:19 +0900

    Feat: 파이프라인 개발

--- src/main/java/com/matzip/api/matzip_api/MatzipApiApplication.java
+++ src/main/java/com/matzip/api/matzip_api/MatzipApiApplication.java
@@ -2,8 +2,10 @@ package com.matzip.api.matzip_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class MatzipApiApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/matzip/api/matzip_api/domain/restrt/controller/RestrtController.java
+++ src/main/java/com/matzip/api/matzip_api/domain/restrt/controller/RestrtController.java
@@ -1,6 +1,5 @@
 package com.matzip.api.matzip_api.domain.restrt.controller;
 
-
 import com.matzip.api.matzip_api.domain.restrt.dto.RestrtDetailResponseDto;
 import com.matzip.api.matzip_api.domain.restrt.service.RestrtService;
 import com.matzip.api.matzip_api.global.CommonResponse;
@@ -9,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,8 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/restrt")
 @RequiredArgsConstructor
 public class RestrtController {
-
     private final RestrtService restrtService;
+
+    @PostMapping("/fetch")
+    public ResponseEntity<String> fetchData() throws Exception {
+        restrtService.fetchData();
+        return ResponseEntity.ok("fetch data successfully.");
+    }
 
     @GetMapping("/{id}")
     public ResponseEntity<CommonResponse<RestrtDetailResponseDto>> getRestrtDetail(@PathVariable Long id) {
